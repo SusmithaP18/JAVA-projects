68038928aacb91aee244bd016f9512362a4b636d
Susmithapadda <paddasusmitha@gmail.com>
2026-02-05 11:03:39 -0600

    Array program travesal

---
+++ src/com/example/Array1.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
