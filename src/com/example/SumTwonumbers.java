1b54bce10360da927d3c3abbc7c916ed8d4efe58
Susmithapadda <susmithapadda18@gmail.com>
2026-02-06 11:11:56 -0600

    different input

--- src/com/example/SumTwonumbers.java
+++ src/com/example/SumTwonumbers.java
@@ -1,4 +1,22 @@
 package com.example;
 
-public class SumTwonumbers {
-}
+import java.util.Scanner;
+    public class SumTwonumbers {
+        public static void main(String[] args) {
+
+            Scanner aa = new Scanner(System.in);
+
+            System.out.print("Enter first number: ");
+            int a = aa.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = aa.nextInt();
+
+            int d = a + b;
+
+
+            System.out.println("d = " + d);
+
+            aa.close();
+        }
+    }
