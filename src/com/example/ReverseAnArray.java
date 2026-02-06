90e2a870340e60c200c4d6fc3ca1b92de2998c9b
Susmithapadda <susmithapadda18@gmail.com>
2026-02-06 11:35:36 -0600

    Add ReverseAnArray program

---
+++ src/com/example/ReverseAnArray.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class ReverseAnArray {
+    public static void main(String[] args){
+
+        int [] arr = { 1,3,5,7};
+
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+
+        }
+    }
+}
