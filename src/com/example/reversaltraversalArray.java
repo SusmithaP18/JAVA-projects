2b74a42cf613a0c91345ab6267c2f5280b3d1926
Susmithapadda <susmithapadda18@gmail.com>
2026-02-06 11:05:46 -0600

    Array program reverse traversal

--- src/com/example/reversaltraversalArray.java
+++ src/com/example/reversaltraversalArray.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class reversaltraversalArray {
     public static void main(String[] args){
-        int[] arr = {1,4,67,8};
+        int[] arr = {1,4,73,8};
         for (int i=arr.length-1; i >=0; i--){
             System.out.println(arr[i]);
         }
