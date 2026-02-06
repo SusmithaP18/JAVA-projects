07ac836d34558d1d2e6aa3f8b43367ba661dfe88
Susmithapadda <paddasusmitha@gmail.com>
2026-02-06 10:47:57 -0600

    Array program reverse travesal

---
+++ src/com/example/reversaltraversalArray.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class reversaltraversalArray {
+    public static void main(String[] args){
+        int[] arr = {1,4,67,8};
+        for (int i=arr.length-1; i >=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
